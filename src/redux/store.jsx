dd21523c6735103a249565b8f28e748e3d82eb5c
mfarhann10 <mfarhann1009@gmail.com>
2025-01-27 01:14:48 +0700

    add a action creator

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -41,13 +41,37 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
+function deposit(ammount) {
+  return { type: "account/deposit", payload: ammount };
+}
+
+function withdraw(ammount) {
+  return {
+    type: "account/withdraw",
+    payload: ammount,
+  };
+}
+
+function requestLoan(ammount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      ammount,
+      purpose,
+    },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
 console.log(store.getState());
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { ammount: 1000, purpose: "Buy a Car" },
-});
+store.dispatch(requestLoan(1000, "Buy a Car"));
 console.log(store.getState());
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
