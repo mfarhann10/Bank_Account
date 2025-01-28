3a795744f7d3bd7dec9e0f3e8fe9975a27d404e8
mfarhann10 <mfarhann1009@gmail.com>
2025-01-28 14:04:17 +0700

    add a redux devtools and fix some bugs

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import accountReducer from "../features/accounts/AccountSlice";
 import customerReducer from "../features/customers/CustomerSlice";
 
@@ -8,6 +9,9 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
